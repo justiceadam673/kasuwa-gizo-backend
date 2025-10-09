93b67917ffc9a31ac38440ec705bd17f115004f6
Justice Adam <justiceadam673@gmail.com>
2025-10-09 13:02:00 +0100

    just pushing the cor settings

--- index.js
+++ index.js
@@ -27,7 +27,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    origin: [
+      process.env.FRONTEND_URL,
+      "http://localhost:3000",
+      "https://gizo-project.netlify.app/",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     // credentials: true,
   })
